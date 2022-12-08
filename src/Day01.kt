c92ea9f671aafb8cfda39141c8ce4fa9fbb316d5
Chris <christhecrisis@gmail.com>
2022-12-08 02:17:51 +0100

    finished day 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,26 @@
+import java.io.File
+
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
+    fun parseInputToData(path:String) = File(path).readText()
+            .split("\n\n")
+            .map {bucket ->
+                bucket.split("\n").sumOf { it.toInt() }
+            }.sortedDescending()
+
+    fun part1(input: List<Int>): Int {
+        return input.first()
     }
 
-    fun part2(input: List<String>): Int {
-        return input.size
+    fun part2(input: List<Int>): Int {
+        return input.take(3).sum()
     }
 
     // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    val testInput = parseInputToData("src/Day01_test.txt")
+    check(part1(testInput) == 24000)
+    check(part2(testInput) == 45000)
 
-    val input = readInput("Day01")
+    val input = parseInputToData("src/Day01.txt")
     println(part1(input))
     println(part2(input))
 }
